db9ef340f9d9101c8ec415bf73e53047285b2982
zentred <87871486+zentred@users.noreply.github.com>
2021-12-30 02:25:15 +0000

    Update t.py
    added a config, and made code ALOT cleaner

--- t.py
+++ t.py
@@ -1,80 +1,71 @@
-import requests, time
+import requests, time, configparser, re, json, threading, ctypes
+from threading import Thread
 req = requests.Session()
 
-lst = []
+completed = []
 
-print('The threshold refers to the percentage. With an input of example "20", if the price goes under 20% of the rap, it will tell you in discord')
-threshold = int(input('Do not include any letters. Enter threshold: '))
-threshold = threshold / 100
-threshold = 1 - threshold
+config = configparser.ConfigParser()
+config.read('config.ini')
 
-blacklist = open('blacklisted.txt','r',encoding='utf-8',errors='ignore').read().splitlines()
+threshhold = 1-(int(config['config']['threshhold'])/100)
+send_projected = config['config']['send_projected']
+discord_id = config['config']['discord_id']
+web = config['config']['webhook']
+minimum_price = int(config['config']['minimum_price'])
+maximum_price = int(config['config']['maximum_price'])
 
-webhook = input('Enter webhook: ')
-discord_id = input('Enter discord ID: ')
+checked = 0
+deals_a = 0
+
+def title():
+    while True:
+        ctypes.windll.kernel32.SetConsoleTitleW(f'Pages Checked: {checked} | Deals Found: {deals_a}')
 
 def deals():
-    # different for each amount of commas because since im splitting by the comma format fucks up if its in the actual limited name
-    r = req.get('https://www.rolimons.com/deals', timeout=4).text.split('item_details = {')[1].split('};')[0].replace('],"', '\n"').splitlines()
-    for line in r:
-        try:
-            format_check = line.split('"')[3]
-            comma_count = format_check.count(',')
-            if comma_count == 0:
-                t = line.replace('"', '').replace(':[', ',').split(',')
-                assetid, assetname, price, rap, projected = t[0], t[1], t[2], t[3], t[5]
-                to_send = f'{assetid}/{assetname}/{price}/{rap}/{projected}'
-                check(to_send)
-            elif comma_count == 1:
-                t = line.replace('"', '').replace(':[', ',').split(',')
-                assetname = t[1] + ',' + t[2]
-                assetid, assetname, price, rap, projected = t[0], assetname, t[3], t[4], t[5]
-                to_send = f'{assetid}/{assetname}/{price}/{rap}/{projected}'
-                check(to_send)
-            elif comma_count == 2:
-                t = line.replace('"', '').replace(':[', ',').split(',')
-                assetname = t[1] + ',' + t[2] + ',' + t[3]
-                assetid, assetname, price, rap, projected = t[0], assetname, t[4], t[5], t[6]
-                to_send = f'{assetid}/{assetname}/{price}/{rap}/{projected}'
-                check(to_send)
-        except Exception as err:
-            print(err)
+    global checked
+    checked += 1
+    r = requests.get('https://www.rolimons.com/deals').text
+    items = re.findall('item_details = (.*?);', r)[0]
+    total = json.loads(items)
+    for x in total:
+        info = total[x]
+        name, price, rap, projected, itemid = info[0], info[1], info[2], info[4], x
+        check(name, price, rap, projected, itemid)
 
-def check(to_send):
-    try:
-        assetid, assetname, price, rap, projected = to_send.split('/',5)
-        try:
-            number = float(price) / float(rap)
-        except: pass
-        else:
-            s = f'{assetid}:{number}'
-            if float(number) <= float(threshold) and number != 0.0 and not s in lst and projected != '1' and not assetid in blacklist:
-                lst.append(s)
-                print(f'{assetid} - {assetname} - {price} - {rap} - {projected}')
-                data = {
-                      'content': f'<@{discord_id}>',
-                      'embeds':[{
-                          'author': {
-                              'name': assetname,
-                              'url': f'https://www.roblox.com/catalog/{assetid}',
-                              },
-                          'color': int('C875C4',16),
-                          'fields': [
-                              {'name': '\u200b','value': f'**Asset ID**: {assetid}\n**Asset Name**: {assetname}\n**Rap**: {rap}\n**Price**: {price}','inline':False},
-                          ],
-                          'thumbnail': {
-                              'url': f'https://www.roblox.com/asset-thumbnail/image?assetId={assetid}&width=420&height=420&format=png',
-                              }
-                      }]
-                    }
-                r = requests.post(webhook, json=data).text
-    except Exception as err:
-        print(err)
+def check(name, price, rap, projected, itemid):
+    global completed
+    if not price == 0 and not rap == 0:
+        drop = int(price) / int(rap)
+        c = f'{itemid}:{drop}'
+        if drop <= threshhold and not c in completed and price >= minimum_price and price <= maximum_price:
+            completed.append(c)
+            if send_projected == 'true':
+                webhook(name, price, rap, projected, itemid)
+            elif send_projected == 'false':
+                if not projected == 1:
+                    webhook(name, price, rap, projected, itemid)
 
-i = 0
+def webhook(name, price, rap, projected, itemid):
+    global deals_a
+    data = {
+      'content': f'<@{discord_id}>',
+      'embeds':[{
+          'author': {
+              'name': name,
+              'url': f'https://www.roblox.com/catalog/{itemid}',
+              },
+          'fields': [
+              {'name': '\u200b','value': f'**Asset ID**: {itemid}\n**Asset Name**: {name}\n**Rap**: {rap}\n**Price**: {price}','inline':False},
+          ],
+          'thumbnail': {
+              'url': f'https://www.roblox.com/asset-thumbnail/image?assetId={itemid}&width=420&height=420&format=png',
+              }
+      }]
+    }
+    requests.post(web, json=data).text
+    deals_a += 1
 
+Thread(target=title).start()
 while True:
-    i += 1
     deals()
-    print(f'Checked deals page - {i}')
     time.sleep(5)
